9027697769c795b1d9204fa98d8017465c30a42c
Bruno Silva <Brunomac@Brunos-MacBook-Pro.local>
2019-11-04 00:09:00 +0000

    update

--- TPC5/Alunos/gestaoAlunos/public/javascripts/apagar.js
+++ TPC5/Alunos/gestaoAlunos/public/javascripts/apagar.js
@@ -1,6 +1,6 @@
 function apagaAluno(ident){
     console.log('Vou apagar o aluno: ' + ident)
     axios.delete('/Alunos/' + ident)
-        .then(response => window.location.assign('/alunos'))
+        .then(response => window.location.assign('/Alunos'))
         .catch(error => console.log(error))
 }
--- TPC5/Alunos/gestaoAlunos/routes/index.js
+++ TPC5/Alunos/gestaoAlunos/routes/index.js
@@ -17,4 +17,24 @@ router.get('/Alunos', function(req, res) {
   }) 
 })
 
+router.get('/Alunos/:idAluno', function(req, res) {
+  var id = req.params.idAluno
+  jsonfile.readFile(myBD, (erro, alunos) => {
+    if(!erro){
+        var index = alunos.findIndex(a => a.Id == id)
+        console.log(index)
+        if(index > -1){
+           var aluno = alunos[index]
+           console.log(aluno)
+           res.render('student', {person : aluno})
+        }
+        else{
+          console.log('Aluno not found!')
+          res.render('error', {error : erro})
+        }            
+    }
+  }) 
+})
+
+
 module.exports = router;
