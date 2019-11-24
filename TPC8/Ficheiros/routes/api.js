d26c5b79fb80e431a5dab31db3275fc37324b43e
Bruno Silva <Brunomac@Brunos-MacBook-Pro.local>
2019-11-24 22:28:39 +0000

    Ficheiros

---
+++ TPC8/Ficheiros/routes/api.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs')
+var Ficheiros = require('../controllers/ficheiros')
+var Ficheiro = require('../models/ficheiro')
+
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+/* GET da lista de ficheiros */
+router.get('/ficheiros', function(req, res) {
+    Ficheiros.listar()
+      .then(dados => res.jsonp(dados))
+      .catch(erro => res.status(500).jsonp(erro))
+})
+
+
+// Falta apenas gravar a imagem na pasta correta, no caso em que o ficeiro é uma imagem.
+router.post('/ficheiros', upload.array('ficheiro'), function(req,res){
+    var listaErros = []
+    
+    for(let i=0; i < req.files.length; i++){
+        let oldPath = __dirname + '/../' + req.files[i].path
+        let newPath = __dirname + '/../public/ficheiros/' + req.files[i].originalname
+
+        fs.rename(oldPath, newPath, function (err) {
+            if (err) throw err
+        })
+
+        let data = new Date()
+
+        let novoFicheiro = new Ficheiro(
+            { 
+                data: data.toISOString(),
+                desc: req.body.desc[i],
+                name: req.files[i].originalname,
+                path: newPath, 
+                mimetype: req.files[i].mimetype, 
+                size: req.files[i].size
+            })
+
+        novoFicheiro.save(function (err, ficheiro) {
+            if (err) listaErros.push(err)
+            else console.log('Ficheiro ' + ficheiro.name + ' foi gravado com sucesso')
+            
+        })
+    }
+    res.redirect('/')
+})
+
+module.exports = router;
